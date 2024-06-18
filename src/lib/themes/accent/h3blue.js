eae2d7773bf2db996c770bd4191089354e85734b
h3ldll <167717217+gvalue-04@users.noreply.github.com>
2024-06-18 17:06:11 -0400

    Update and rename highcontrast.js to h3blue.js

---
+++ src/lib/themes/accent/h3blue.js
@@ -0,0 +1,27 @@
+const guiColors = {
+    'motion-primary': '#00d27c',
+    'motion-primary-transparent': '#00d27ce6',
+    'motion-tertiary': '#00d27c',
+
+    'looks-secondary': '#00d27c',
+    'looks-transparent': '#00d27c59',
+    'looks-light-transparent': '#00d27c26',
+    'looks-secondary-dark': 'hsla(0, 42%, 51%, 1)',
+
+    'extensions-primary': 'hsla(10, 85%, 65%, 1)',
+    'extensions-tertiary': 'hsla(10, 85%, 40%, 1)',
+    'extensions-transparent': 'hsla(10, 85%, 65%, 0.35)',
+    'extensions-light': 'hsla(10, 57%, 85%, 1)',
+
+    'drop-highlight': '#00d27c'
+};
+
+const blockColors = {
+    checkboxActiveBackground: '#00d27c',
+    checkboxActiveBorder: '#00d27c'
+};
+
+export {
+    guiColors,
+    blockColors
+};
