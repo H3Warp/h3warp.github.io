0022658800b9d95caae93a5057bc6166236bcc19
h3ldll <167717217+gvalue-04@users.noreply.github.com>
2024-06-18 17:01:18 -0400

    Update and rename red.js to highcontrast.js

---
+++ src/lib/themes/accent/highcontrast.js
@@ -0,0 +1,29 @@
+const guiColors = {
+    'motion-primary': '#81b2ff',
+    'motion-primary-transparent': '#81b2ffe6',
+    'motion-tertiary': '#81b2ff',
+
+    'looks-secondary': '#81b2ff',
+    'looks-transparent': '#81b2ff59',
+    'looks-light-transparent': '#81b2ff26',
+    'looks-secondary-dark': 'hsla(0, 42%, 51%, 1)',
+
+    'extensions-primary': 'hsla(10, 85%, 65%, 1)',
+    'extensions-tertiary': 'hsla(10, 85%, 40%, 1)',
+    'extensions-transparent': 'hsla(10, 85%, 65%, 0.35)',
+    'extensions-light': 'hsla(10, 57%, 85%, 1)',
+
+    'drop-highlight': '#81b2ff',
+
+    'text-color': '#000000'
+};
+
+const blockColors = {
+    checkboxActiveBackground: '#81b2ff',
+    checkboxActiveBorder: '#81b2ff'
+};
+
+export {
+    guiColors,
+    blockColors
+};
